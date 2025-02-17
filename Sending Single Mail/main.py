0c5d9e94c08b2ed56a6257c4f1ead33d6924d6d6
JonhIsh <rodriguezjonhy04@gmail.com>
2025-02-17 12:51:08 +0000

    Changed script so it sends emails at a certain time of day

--- Sending Single Mail/main.py
+++ Sending Single Mail/main.py
@@ -1,5 +1,7 @@
 import yagmail
 import os
+import time
+from datetime import datetime as dt
 
 sender = os.getenv('email_address')
 receiver = "nebayor992@kytstore.com"
@@ -10,5 +12,10 @@ contents = """
 Hello, this is a test email.
 """
 
-yag = yagmail.SMTP(user=sender, password=os.getenv('email_password'))
-yag.send(to=receiver, subject=subject, contents=contents)
+
+while True:
+    now = dt.now()
+    if now.hour == 12 and now.minute == 48:
+        yag = yagmail.SMTP(user=sender, password=os.getenv('email_password'))
+        yag.send(to=receiver, subject=subject, contents=contents)
+        time.sleep(60)
